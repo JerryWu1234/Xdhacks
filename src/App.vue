bf0ff221f9eb7d986d8873dba31fa8eba5f9562a
林胜吴 <409187100@qq.com>
2019-10-20 14:00:37 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
+    <div v-show="$route.path !== '/'">
+      <van-tabbar v-model="active">
+
+        <van-tabbar-item name="home" @click="$router.push('/helloword')" icon="home-o">周围商铺</van-tabbar-item>
+        <van-tabbar-item name="like" @click="$router.push('/preference')" icon="like-o">贴心推荐</van-tabbar-item>
+        <van-tabbar-item name="setting" @click="$router.push('/person')" icon="setting">个人中心</van-tabbar-item>
+      </van-tabbar>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                active: "home",
+            }
+        },
+        mounted() {
+            console.log()
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background: rgba(0, 0, 0, 0);
+    overflow: hidden;
+    margin: auto 0;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    margin: 0 auto;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div >
+    <van-nav-bar
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+    />
+    <div>
+      <van-dropdown-menu>
+        <van-dropdown-item v-model="value1" :options="option1" />
+        <van-dropdown-item v-model="value2" :options="option2" />
+      </van-dropdown-menu>
+      <div>
+        <div v-for="(val, item) in list" :key="item">
+          <van-card
+              :tag="val.tag"
+              :desc="val.desc"
+              :title="val.title"
+              thumb="https://img.yzcdn.cn/vant/t-thirt.jpg"
+          >
+            <div slot="tags">
+              <van-tag plain type="danger">
+                <van-rate
+                  v-model="valuestart"
+                  :size="10"
+                  color="#ee0a24"
+                  void-icon="star"
+                  void-color="#eee"
+              />
+                <span>4.5</span>
+              </van-tag>
+              <van-tag plain type="danger">促销8折</van-tag>
+              <div>均价:￥{{val.price}}/每条</div>
+            </div>
+            <div slot="footer">
+              <van-button @click="opendialog" size="mini">200m</van-button>
+              <!--<van-button size="mini">按钮</van-button>-->
+            </div>
+          </van-card>
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        methods: {
+            onClickLeft() {
+
+            },
+            opendialog(){
+                this.$dialog.confirm({
+                    message: '是否需要跳转导航App'
+                }).catch((res) =>{
+                    console.log(res)
+                })
+            }
+        },
+        data() {
+            return {
+                valuestart:4,
+                value1: 0,
+                value2: 'a',
+                option1: [
+                    {text: '全部商铺', value: 0},
+                    {text: '休闲装', value: 1},
+                    {text: '商务装', value: 2},
+                    {text: '运动装', value: 3},
+                ],
+                option2: [
+                    {text: '默认排序', value: 'a'},
+                    {text: '人气优先', value: 'b'},
+                    {text: '好评优先', value: 'c'},
+                ],
+                list: [
+                    {
+                        tag: '商务',
+                        desc: '合生汇国际广场L2-43',
+                        title: 'GUCCI',
+                        price: 1000
+                    },
+                    {
+                        tag: '休闲',
+                        desc: '合生汇国际广场L2-44',
+                        title: '森马',
+                        price: 100
+                    },
+                    {
+                        tag: '运动',
+                        desc: '合生汇国际广场L2-40',
+                        title: 'nike',
+                        price: 400
+                    },
+                    {
+                        tag: '运动',
+                        desc: '合生汇国际广场L2-40',
+                        title: '阿迪达斯',
+                        price: 300
+                    },
+
+                ]
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .divcontant{
+
+  }
 </style>
